8c7ad397965a65d324d56f87313ab36b34fe9b10
Mauro Casse <mnkeis@gmail.com>
2022-08-08 12:11:23 -0300

    Added tests for tokens post route

--- dart_server/test/routes/api/tokens/index_test.dart
+++ dart_server/test/routes/api/tokens/index_test.dart
@@ -1,30 +1,63 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
+import 'package:user_repository/user_repository.dart';
 
 import '../../../../routes/api/tokens/index.dart' as route;
 
 class _MockRequestContext extends Mock implements RequestContext {}
 
+class _MockUserApi extends Mock implements UserApi {}
+
 void main() {
+  const secret = 'my-secret';
   const username = '__test_username__';
   const password = '__test_password__';
+  final context = _MockRequestContext();
+  final userApi = _MockUserApi();
+  final body = <String, dynamic>{
+    'username': username,
+    'password': password,
+  };
+  const user = User(
+    username: 'test_username',
+    email: 'test@email.com',
+    name: 'testName',
+    lastName: 'testLastName',
+  );
+  final userRepository = UserRepository(userApi);
   group('Tokens', () {
-    test('create a new token', () {
-      final context = _MockRequestContext();
+    test('create a new token', () async {
+      final jwt = JWT(user);
+      final token = jwt.sign(SecretKey(secret));
+      Handler middleware(Handler handler) {
+        return handler
+            .use(provider<String>((context) => secret))
+            .use(provider<UserRepository>((context) => userRepository));
+      }
+
+      final handler = const Pipeline()
+          .addMiddleware(middleware)
+          .addHandler(route.onRequest);
+
       final request = Request.post(
         Uri.parse('http://localhost/api/tokens'),
-        body: '''
-        {
-          'username': $username,
-          'password': $password,
-        }''',
+        body: jsonEncode(body),
       );
       when(() => context.request).thenReturn(request);
-      final response = route.onRequest(context);
+      when(
+        () => userApi.getUserWithUsernameAndPassword(
+          username: username,
+          password: password,
+        ),
+      ).thenAnswer((_) async => user);
+      final response = await handler(context);
       expect(response.statusCode, equals(HttpStatus.ok));
+      expect(response.json(), completion({'token': token}));
     });
   });
 }
