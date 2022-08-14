0cde4ca5e34fe42637547aa61d2795ec748ccb75
Mauro Casse <mnkeis@gmail.com>
2022-08-14 19:02:04 -0300

    UserApi implementation with PostgreSQL database

--- packages/pg_user_api/lib/src/pg_user_api.dart
+++ packages/pg_user_api/lib/src/pg_user_api.dart
@@ -1,7 +1,96 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+import 'package:postgres/postgres.dart';
+import 'package:user_api/user_api.dart';
+
 /// {@template pg_user_api}
 /// A Very Good Project created by Very Good CLI.
 /// {@endtemplate}
-class PgUserApi {
+class PgUserApi implements UserApi {
   /// {@macro pg_user_api}
-  const PgUserApi();
+  const PgUserApi(this._pg);
+
+  final PostgreSQLConnection _pg;
+
+  @override
+  Future<User?> getUserWithUsernameAndPassword({
+    required String username,
+    required String password,
+  }) async {
+    await _pg.open();
+    final hashedPassword = sha256.convert(utf8.encode(password));
+    final dbResult = await _pg.mappedResultsQuery(
+      '''
+        SELECT * FROM users 
+        WHERE username = @username AND hasedPassword = @hashedPassword''',
+      substitutionValues: {
+        'username': username,
+        'hashedPassword': base64.encode(hashedPassword.bytes),
+      },
+    );
+    if (dbResult.isEmpty) {
+      return null;
+    }
+
+    if (dbResult.length > 1) {
+      throw PostgreSQLException('invalid-result-set');
+    }
+    final row = dbResult.first.values.first;
+
+    try {
+      return User.fromJson(row);
+    } catch (_) {
+      throw const FormatException();
+    }
+  }
+
+  @override
+  Future<User?> getUserWithRefreshToken(String refreshToken) async {
+    await _pg.open();
+    final result = await _pg.mappedResultsQuery(
+      '''
+      SELECT * FROM users WHERE @refreshToken = ANY(refresh_token)''',
+      substitutionValues: {
+        'refreshToken': refreshToken,
+      },
+    );
+
+    if (result.isEmpty) {
+      return null;
+    }
+
+    return User.fromJson(result.first.values.first);
+  }
+
+  @override
+  Future<User> getUserById(int id) async {
+    await _pg.open();
+    final result = await _pg.mappedResultsQuery(
+      '''
+      SELECT * FROM users WHERE id = @id''',
+      substitutionValues: {'id': id},
+    );
+
+    if (result.isEmpty) {
+      throw PostgreSQLException('invalid-index');
+    }
+
+    return User.fromJson(result.first.values.first);
+  }
+
+  @override
+  Future<void> updateUser(User user) async {
+    await _pg.open();
+    await _pg.query(
+      '''
+        UPDATE users SET 
+          (
+            email = @email, name = @name, phone = @phone, 
+            photo_url = @photoUrl, roles = @roles, 
+            refresh_tokens = @refreshTokens
+          )''',
+      substitutionValues: user.toJson(),
+    );
+  }
 }
--- packages/pg_user_api/test/src/pg_user_api_test.dart
+++ packages/pg_user_api/test/src/pg_user_api_test.dart
@@ -1,11 +1,264 @@
 // ignore_for_file: prefer_const_constructors
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:pg_user_api/pg_user_api.dart';
+import 'package:postgres/postgres.dart';
 import 'package:test/test.dart';
+import 'package:user_api/user_api.dart';
+
+class _MockPostgreSQLConnection extends Mock implements PostgreSQLConnection {}
 
 void main() {
+  const username = '__test_username__';
+  const password = '__test_password__';
+  const email = 'user@example.com';
+  const givenName = '__test_given_name__';
+  const phone = '__test_phone_number';
+  final pgConnection = _MockPostgreSQLConnection();
   group('PgUserApi', () {
     test('can be instantiated', () {
-      expect(PgUserApi(), isNotNull);
+      expect(PgUserApi(pgConnection), isNotNull);
+    });
+
+    group('method getUserWithUsernameAndPassword', () {
+      test('returns a valid user', () async {
+        final hashedPassword = sha256.convert(utf8.encode(password));
+        when(pgConnection.open).thenAnswer((_) async => null);
+        when(
+          () => pgConnection.mappedResultsQuery(
+            any(),
+            substitutionValues: {
+              'username': username,
+              'hashedPassword': base64.encode(hashedPassword.bytes),
+            },
+          ),
+        ).thenAnswer(
+          (_) async => [
+            {
+              'users': {
+                'id': 1,
+                'username': username,
+                'email': email,
+                'name': givenName,
+                'phone': phone,
+              }
+            }
+          ],
+        );
+        final userApi = PgUserApi(pgConnection);
+        final user = await userApi.getUserWithUsernameAndPassword(
+          username: username,
+          password: password,
+        );
+        expect(
+          user,
+          User(
+            id: 1,
+            username: username,
+            email: email,
+            name: givenName,
+            phone: phone,
+          ),
+        );
+      });
+
+      test('throws UserNotFoundException when invalid username or password',
+          () async {
+        final hashedPassword = sha256.convert(utf8.encode(password));
+        when(pgConnection.open).thenAnswer((_) async => null);
+        when(
+          () => pgConnection.mappedResultsQuery(
+            any(),
+            substitutionValues: {
+              'username': username,
+              'hashedPassword': base64.encode(hashedPassword.bytes),
+            },
+          ),
+        ).thenAnswer(
+          (_) async => [],
+        );
+        final userApi = PgUserApi(pgConnection);
+        final result = userApi.getUserWithUsernameAndPassword(
+          username: username,
+          password: password,
+        );
+        await expectLater(result, completion(null));
+      });
+
+      test('throws PostgreSQLException if db returns more than one row',
+          () async {
+        final hashedPassword = sha256.convert(utf8.encode(password));
+        when(pgConnection.open).thenAnswer((_) async => null);
+        when(
+          () => pgConnection.mappedResultsQuery(
+            any(),
+            substitutionValues: {
+              'username': username,
+              'hashedPassword': base64.encode(hashedPassword.bytes),
+            },
+          ),
+        ).thenAnswer(
+          (_) async => [
+            {
+              'users': {'username': username}
+            },
+            {
+              'users': {'username': username}
+            }
+          ],
+        );
+        final userApi = PgUserApi(pgConnection);
+        final result = userApi.getUserWithUsernameAndPassword(
+          username: username,
+          password: password,
+        );
+        await expectLater(result, throwsA(isA<PostgreSQLException>()));
+      });
+
+      test('throws FormatException if db returns an invalid result', () async {
+        final hashedPassword = sha256.convert(utf8.encode(password));
+        when(pgConnection.open).thenAnswer((_) async => null);
+        when(
+          () => pgConnection.mappedResultsQuery(
+            any(),
+            substitutionValues: {
+              'username': username,
+              'hashedPassword': base64.encode(hashedPassword.bytes),
+            },
+          ),
+        ).thenAnswer(
+          (_) async => [
+            {
+              'users': {'username': username}
+            },
+          ],
+        );
+        final userApi = PgUserApi(pgConnection);
+        final result = userApi.getUserWithUsernameAndPassword(
+          username: username,
+          password: password,
+        );
+        await expectLater(result, throwsA(isA<FormatException>()));
+      });
+    });
+
+    group('method getUserWithRefreshToken', () {
+      test('returns a user if the token is valid', () async {
+        const refreshToken = 'some-refresh-token';
+        when(pgConnection.open).thenAnswer((_) async => null);
+        when(
+          () => pgConnection.mappedResultsQuery(
+            any(),
+            substitutionValues: {
+              'refreshToken': refreshToken,
+            },
+          ),
+        ).thenAnswer(
+          (_) async => [
+            {
+              'users': {
+                'id': 1,
+                'username': username,
+                'email': email,
+                'name': givenName,
+                'phone': phone,
+              }
+            }
+          ],
+        );
+        final userApi = PgUserApi(pgConnection);
+        final user = await userApi.getUserWithRefreshToken(refreshToken);
+        expect(
+          user,
+          User(
+            id: 1,
+            username: username,
+            email: email,
+            name: givenName,
+            phone: phone,
+          ),
+        );
+      });
+
+      test('returns null if the token is not found', () async {
+        const refreshToken = 'some-refresh-token';
+        when(pgConnection.open).thenAnswer((_) async => null);
+        when(
+          () => pgConnection.mappedResultsQuery(
+            any(),
+            substitutionValues: {
+              'refreshToken': refreshToken,
+            },
+          ),
+        ).thenAnswer(
+          (_) async => [],
+        );
+        final userApi = PgUserApi(pgConnection);
+        final user = await userApi.getUserWithRefreshToken(refreshToken);
+        expect(
+          user,
+          isNull,
+        );
+      });
+    });
+
+    group('method getUserById', () {
+      test('returns a user if id is valid', () async {
+        when(pgConnection.open).thenAnswer((_) async => null);
+        when(
+          () => pgConnection.mappedResultsQuery(
+            any(),
+            substitutionValues: {
+              'id': 1,
+            },
+          ),
+        ).thenAnswer(
+          (_) async => [
+            {
+              'users': {
+                'id': 1,
+                'username': username,
+                'email': email,
+                'name': givenName,
+                'phone': phone,
+              }
+            }
+          ],
+        );
+        final userApi = PgUserApi(pgConnection);
+        final user = await userApi.getUserById(1);
+        expect(
+          user,
+          User(
+            id: 1,
+            username: username,
+            email: email,
+            name: givenName,
+            phone: phone,
+          ),
+        );
+      });
+
+      test('throws ProgreSQLException if id is not valid', () async {
+        when(pgConnection.open).thenAnswer((_) async => null);
+        when(
+          () => pgConnection.mappedResultsQuery(
+            any(),
+            substitutionValues: {
+              'id': 1,
+            },
+          ),
+        ).thenAnswer(
+          (_) async => [],
+        );
+        final userApi = PgUserApi(pgConnection);
+        expect(
+          userApi.getUserById(1),
+          throwsA(isA<PostgreSQLException>()),
+        );
+      });
     });
   });
 }
