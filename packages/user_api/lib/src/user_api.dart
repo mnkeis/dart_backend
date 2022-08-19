3e75acdca10eb0976fe795b951f10a4908799818
Mauro Casse <mnkeis@gmail.com>
2022-08-19 15:48:28 -0300

    Added inteface methods to create new users, update password and recover password

--- packages/user_api/lib/src/user_api.dart
+++ packages/user_api/lib/src/user_api.dart
@@ -16,9 +16,27 @@ abstract class UserApi {
   /// Search the user owner of a given refresh token
   Future<User?> getUserWithRefreshToken(String refreshToken);
 
+  /// Search the user with his username or email
+  Future<User?> getUserWithUsernameOrEmail(String usernameOrEmail);
+
   /// Get a user by it's id
   Future<User> getUserById(int id);
 
   /// Update user
   Future<void> updateUser(User user);
+
+  /// Update user password
+  Future<void> updateUserPassword({
+    required User user,
+    required String password,
+  });
+
+  /// Verify username
+  Future<bool> verifyUsername(String username);
+
+  /// Inserts a new user
+  Future<bool> createUser({
+    required User user,
+    required String password,
+  });
 }
